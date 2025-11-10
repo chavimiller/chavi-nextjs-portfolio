cc73acc84145147eeb5627e9b9d2f1738b841fcf
chavimiller <chaviweisman@gmail.com>
2025-11-10 11:43:08 +0200

    Edit project information

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
     {
       key: 1,
       title: "Shopping Cart",
-      desc: "This shopping cart app was built with React. It shows a list of products from a mocked API, lets users add and remove items from the cart, and updates the cart’s contents and total price in real time. It also has navigation between a homepage, shop page, and cart page using React Router.",
+      desc: "This shopping cart app was built with React, and is currently only available for desktop-view. It shows a list of products from a mocked API, lets users add and remove items from the cart, and updates the cart's contents and total price in real time. It also has navigation between a homepage, shop page, and cart page using React Router.",
       stack: [
         { name: "React", symbol: "/images/React.svg" },
         { name: "CSS", symbol: "/images/Css.svg" },
@@ -21,7 +21,7 @@ const Projects = () => {
     {
       key: 2,
       title: "Calculator",
-      desc: "This simple calculator app was built using JavaScript and lets you perform basic math operations like addition, subtraction, multiplication, and division. It handles decimals, clearing, deleting digits, and updates the display in real time as you click.",
+      desc: "This simple, responsive calculator app was built using JavaScript and lets you perform basic math operations like addition, subtraction, multiplication, and division. It handles decimals, clearing, deleting digits, and updates the display in real time as you click.",
       stack: [
         { name: "JavaScript", symbol: "/images/Javascript.svg" },
         { name: "HTML", symbol: "/images/Html.svg" },
@@ -34,7 +34,7 @@ const Projects = () => {
     {
       key: 3,
       title: "Weather App",
-      desc: "This app was built with JavaScript, HTML, and CSS, using a modular approach to keep the code clean and components separate. It fetches real-time weather data from an external API, allowing users to search for any city and view current temperature, weather conditions, and other details.",
+      desc: "This app was built with JavaScript, HTML, and CSS, using a modular approach to keep the code clean and components separate. It fetches real-time weather data from an external API, allowing users to search for any city and view current temperature, weather conditions, and other details. Currently it has basic responsiveness and being migrated into a React project.",
       stack: [
         { name: "JavaScript", symbol: "/images/Javascript.svg" },
         { name: "HTML", symbol: "/images/Html.svg" },
@@ -47,7 +47,7 @@ const Projects = () => {
     {
       key: 4,
       title: "To-Do List",
-      desc: "This to-do list app is divided into modules to make the code easier to follow and maintain. It demonstrates handling events, updating the interface dynamically, manipulating the DOM, and saving tasks to local storage so they remain after page refreshes.",
+      desc: "Currently only available for desktop-view, this to-do list app is divided into modules to make the code easier to follow and maintain. It demonstrates handling events, updating the interface dynamically, manipulating the DOM, and saving tasks to local storage so they remain after page refreshes.",
       stack: [
         { name: "JavaScript", symbol: "/images/Javascript.svg" },
         { name: "HTML", symbol: "/images/Html.svg" },
